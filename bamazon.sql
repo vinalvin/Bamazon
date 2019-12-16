c590aaa18d89b0b4cfa401a245dd2f0d9510eb89
Junming Zhang <vinalvin1010@gmail.com>
2019-12-15 23:53:29 -0800

    Filed the db

--- bamazon.sql
+++ bamazon.sql
@@ -11,3 +11,8 @@ price DECIMAL (8,2),
 stock_quantity INT NOT NULL,
 PRIMARY KEY(item_id)
 );
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES
+('1001', 'UGG Gift Cards', 'Gift Cards', '100', '250'),
+('2002', 'Levis Skinny', 'Clothing', '100', '300'),
+('3003', 'Kate Spade Bags', 'Bags', '150', '200');
